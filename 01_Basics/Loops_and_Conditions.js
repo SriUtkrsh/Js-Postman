68f2bf76ef2cd91cf61608712445cb287cd5940e
Utkarsh Srivastava <149072298+SriUtkrsh@users.noreply.github.com>
2024-05-18 10:44:12 +0000

    If_while_doWhile done

---
+++ 01_Basics/Loops_and_Conditions.js
@@ -0,0 +1,27 @@
+const flag =true
+
+if(flag)
+    {
+        console.log("Condition Satisfied")
+
+    }
+    else
+    {
+        console.log("condition not Satisfied")
+
+    }
+    
+    let i=0
+    while(i>10)
+        {
+            i++
+            console.log(i)
+
+        }
+
+do
+{
+i++
+}  
+while(i>10) ;
+    console.log(i)     
