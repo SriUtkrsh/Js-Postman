493af5ee561dd9d4a76d364d5388073cc5201f4e
Utkarsh Srivastava <149072298+SriUtkrsh@users.noreply.github.com>
2024-05-18 10:19:48 +0000

    First commit

--- 01_Basics/Test.js
+++ 01_Basics/Test.js
@@ -1,1 +1,29 @@
 console.log("Hello JavaScripts")
+
+console.log("Hello Javascript")
+
+//Comments
+
+/*
+   This is multiline comment
+*/
+
+// variable Declrations and Datatypes
+
+var a=4  //number
+let b= 7
+console.log (b)
+console.log (typeof(b))
+
+//String
+let c = "Utkarsh Srivastava"
+console.log(typeof(c))
+
+//Booolean
+let required = true
+console.log(typeof(required))
+//null and undefined
+
+//not operator (!)
+console.log(!required)
+// var (let , const)ES6
