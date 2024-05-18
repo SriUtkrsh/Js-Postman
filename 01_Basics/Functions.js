ed68d6f77d2a25f5ebaed5b7ceeb7338a69ec003
Utkarsh Srivastava <149072298+SriUtkrsh@users.noreply.github.com>
2024-05-18 17:40:19 +0000

    Functions done

---
+++ 01_Basics/Functions.js
@@ -0,0 +1,19 @@
+function add(a,b)
+{
+   return a+b
+}
+let sum = add(2,3)
+console.log(sum)
+
+
+
+//Anonymous function: function do not have name
+
+let SumofInteger =function(c,d)
+{
+    return c+d
+}
+
+
+let SumofNumbers =(c,d)=>c+d
+console.log(SumofNumbers(5,5))
