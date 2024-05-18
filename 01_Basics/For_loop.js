dc29b48a4f2efeb5e53cea946b351c20e46d7787
Utkarsh Srivastava <149072298+SriUtkrsh@users.noreply.github.com>
2024-05-18 11:13:12 +0000

    For Loop done

---
+++ 01_Basics/For_loop.js
@@ -0,0 +1,14 @@
+let n=0
+for(let k=0;k<=100;k++)
+{
+   
+    if(k%2 == 0 && k%5 == 0)
+        {
+            n++
+            console.log(k)
+            if(n==10)
+                break
+        }
+        
+ 
+}
